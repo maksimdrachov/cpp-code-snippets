08d0c4ce2c687d6d82d773e684b43efa89b1971b
Maksim Drachov <maksim.drachov@outlook.com>
2022-09-11 14:42:27 +0300

    enum update

--- 2-enum/main.cpp
+++ 2-enum/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <string>
 
 enum LightState {
     Off,
     Low,
     Medium,
     High
+};
+
+std::string LightStateString[4] = {
+    "Off",
+    "Low",
+    "Medium",
+    "High"
 };
 
 int main() {
@@ -17,6 +25,13 @@ int main() {
     std::cout << LightLow << std::endl;
     std::cout << LightMedium << std::endl;
     std::cout << LightHigh << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << LightStateString[LightOff] << std::endl;
+    std::cout << LightStateString[LightLow] << std::endl;
+    std::cout << LightStateString[LightMedium] << std::endl;
+    std::cout << LightStateString[LightHigh] << std::endl;
 
     return EXIT_SUCCESS;
 }
